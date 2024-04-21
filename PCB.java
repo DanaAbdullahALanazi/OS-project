219b7396126e8b8fdbfff79d85baa7df5fda1565
Abrar <145024980+AbrarTA@users.noreply.github.com>
2024-04-21 11:07:50 +0300

    SJF Scheduling

--- PCB.java
+++ PCB.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PCB {
 private String ProcessID;
 private int processPriority;
@@ -13,13 +15,45 @@ public PCB(String ProcessID,double cpuBurstTime,double startingTime,double termi
     this.ProcessID=ProcessID;
     this.cpuBurstTime=cpuBurstTime;
     this.startingTime=startingTime;
-    this.terminationTime=terminationTime;
-    this.turnAroundTime=terminationTime-arrivalTime;
-    this.waitingTime=turnAroundTime-cpuBurstTime;
-    this.performanceTime=startingTime-arrivalTime;
+    this.terminationTime = 0;
+    this.turnAroundTime = 0;
+    this.waitingTime = 0;
+    this.performanceTime = 0;
+    //this.terminationTime=terminationTime;
+    //this.turnAroundTime=terminationTime-arrivalTime;
+    //this.waitingTime=turnAroundTime-cpuBurstTime;
+    //this.performanceTime=startingTime-arrivalTime;
 }
 
 
+public void SJFScheduling( List <PCB> Q2){
+        
+    double currentTime = 0;
+
+    //sort each process depending on it's Burst time 
+    Collections.sort(Q2, new Comparator<PCB>() {
+        @Override
+        public int compare(PCB p1, PCB p2) {
+            return Double.compare(p1.cpuBurstTime, p2.cpuBurstTime);
+        }
+    });
+
+    // loop through processes
+    for (PCB process : Q2){
+        //the start time for the process
+        process.startingTime = currentTime;
+
+        currentTime += process.getCpuBurstTime();
+
+        //calculate termination time, performance time, turn around time and waiting time
+        process.terminationTime = currentTime;
+        process.performanceTime = process.startingTime - process.arrivalTime;
+        process.turnAroundTime = process.terminationTime - process.arrivalTime;
+        process.waitingTime = process.turnAroundTime - process.cpuBurstTime;
+    }
+
+
+    }
 
 
 
