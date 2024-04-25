98ea01cac225c03cf24b5fb3db84dfe6c80ae24c
DanaHafez <156857033+2DH0@users.noreply.github.com>
2024-04-25 19:48:52 +0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -12,46 +12,46 @@ public static Queue<PCB> cpuExecutionQueue = new LinkedList<>();
 public static Queue<PCB> Queue2 = new LinkedList<>();
 
 public static void main(String[] args) {
-     System.out.println("Welcome to the process scheduling program!");
-     int choice ;
-     int processesNum;
-     int priorityLvl ;
-     double processArrivalTime;
-     double burstTime;
-     PCB newProcess;
-     Queue<PCB> readyQueue = new LinkedList<>();
-     Queue<PCB> Queue1 = new LinkedList<>();
+    System.out.println("Welcome to the process scheduling program!");
+    int choice ;
+    int processesNum;
+    int priorityLvl ;
+    double processArrivalTime;
+    double burstTime;
+    PCB newProcess;
+    Queue<PCB> readyQueue = new LinkedList<>();
+    Queue<PCB> Queue1 = new LinkedList<>();
      //Queue<PCB> Queue2 = new LinkedList<>();
-     do{
-          System.out.println("Please choose an option:");
-          System.out.println("1. Enter process' information.");
-          System.out.println("2. Displaying a detailed report about each process\nand
-           diffrenet shculing critera.");
-           System.out.println("3. Exit the program.");
-           System.out.print("Enter your choice:");
+    do{
+        System.out.println("Please choose an option:");
+        System.out.println("1. Enter process' information.");
+        System.out.println("2. Displaying a detailed report about each process\nand
+        diffrenet shculing critera.");
+        System.out.println("3. Exit the program.");
+        System.out.print("Enter your choice:");
 			choice = input.nextInt();
 			input.nextLine();// for garbage
-          
-               switch(choice){
+    
+            switch(choice){
                     
                     case 1 : //entering process' info
                     System.out.println("Please enter the number of processes to be scheduled:");
                     processesNum = input.nextInt();
-                     for(int i = 1 ; i<=processesNum ; i++){
-                         System.out.println("Please enter the priority of process "+i+" :");
-                         priorityLvl = input.nextInt();
-                         System.out.println("Please enter the arrival time of process "+i+" :");
-                         processArrivalTime = input.nextDouble();
-                         System.out.println("Please enter the CPU burst of process "+i+" :");
-                         burstTime = input.nextDouble();
-                         
+                    for(int i = 1 ; i<=processesNum ; i++){
+                        System.out.println("Please enter the priority of process "+i+" :");
+                        priorityLvl = input.nextInt();
+                        System.out.println("Please enter the arrival time of process "+i+" :");
+                        processArrivalTime = input.nextDouble();
+                        System.out.println("Please enter the CPU burst of process "+i+" :");
+                        burstTime = input.nextDouble();
+                        
                          //creating a pcb object to schedule it
-                         newProcess= new PCB(priorityLvl,processArrivalTime,burstTime);
-                         newProcess.setID("P"+i);
-                         readyQueue.add(newProcess);
-                         primitivePriorityScheduling(readyQueue);
+                        newProcess= new PCB(priorityLvl,processArrivalTime,burstTime);
+                        newProcess.setID("P"+i);
+                        readyQueue.add(newProcess);
+                        primitivePriorityScheduling(readyQueue);
                          //add schudling algorthim call
-                         }//end for
+                        }//end for
 
                     break;
 
@@ -63,16 +63,16 @@ public static void main(String[] args) {
 
                     break;
 
-               
+            
 
                     default:
 				System.out.println("Wrong input! try again" + "\n");
 
-               }
+            }
 
 
 
-     }while(choice != 3);
+    }while(choice != 3);
 }//end main
 
 
@@ -142,7 +142,7 @@ public static void SJFScheduling(){
         //add each process after execution to cpuExecutionQueue 
         cpuExecutionQueue.add(process);
 
-       }
+    }
 
 
 }
@@ -162,81 +162,81 @@ for (PCB process : readyQueue) {
 
 
      public static void processesReport(Queue<PCB> queue1, Queue<PCB> queue2){ //P1|P2 add 
-          double totalTurnaround = 0 ;
-          double totalWaitingTime =0;
-          double totalResponseTime =0;
-          double numberOfProcesses = queue1.size()+queue2.size();
+        double totalTurnaround = 0 ;
+        double totalWaitingTime =0;
+        double totalResponseTime =0;
+        double numberOfProcesses = queue1.size()+queue2.size();
             try {
-                 
+                
                // Delete the existing file if it exists
-                  File file = new File("Report.txt");
-                  if (file.exists()) {
-                      file.delete();
-                  }
+                File file = new File("Report.txt");
+                if (file.exists()) {
+                    file.delete();
+                }
                   // Open the file for writing
-                  FileWriter fileWriter = new FileWriter("Report.txt");
-                  BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-     
+                FileWriter fileWriter = new FileWriter("Report.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+    
                   // Write and print data from queue1 RR
-                  for (PCB pcb : queue1) {
-                      String data = "Process ID: " + pcb.getProcessID() +
-                             ", Process Priority: " + pcb.getProcessPriority() +
-                             ", Arrival Time: " + pcb.getArrivalTime()+"ms" +
-                             ", CPU Burst Time: " + pcb.getCpuBurstTime() +"ms" +
-                             ", Starting Time: " + pcb.getStartingTime()+"ms" +
-                             ", Termination Time: " + pcb.getTerminationTime()+"ms" +
-                             ", Turnaround Time: " + pcb.getTurnAroundTime()+"ms" +
-                             ", Waiting Time: " + pcb.getWaitingTime()+"ms" +
-                             ", Response Time: " + pcb.getPerformanceTime() +"ms";
-                      bufferedWriter.write(data);
-                      bufferedWriter.newLine();
+                for (PCB pcb : queue1) {
+                    String data = "Process ID: " + pcb.getProcessID() +
+                            ", Process Priority: " + pcb.getProcessPriority() +
+                            ", Arrival Time: " + pcb.getArrivalTime()+"ms" +
+                            ", CPU Burst Time: " + pcb.getCpuBurstTime() +"ms" +
+                            ", Starting Time: " + pcb.getStartingTime()+"ms" +
+                            ", Termination Time: " + pcb.getTerminationTime()+"ms" +
+                            ", Turnaround Time: " + pcb.getTurnAroundTime()+"ms" +
+                            ", Waiting Time: " + pcb.getWaitingTime()+"ms" +
+                            ", Response Time: " + pcb.getPerformanceTime() +"ms";
+                    bufferedWriter.write(data);
+                    bufferedWriter.newLine();
                       System.out.println(data); // Print to console
-                      totalTurnaround += pcb.getTurnAroundTime();
-                      totalWaitingTime += pcb.getWaitingTime();
-                      totalResponseTime += pcb.getPerformanceTime();
-                     
-                      
-                      
-                  }//end for
-     
+                    totalTurnaround += pcb.getTurnAroundTime();
+                    totalWaitingTime += pcb.getWaitingTime();
+                    totalResponseTime += pcb.getPerformanceTime();
+                    
+                    
+                    
+                }//end for
+    
                   // Write and print data from queue2 SJF
-                  for (PCB pcb : queue2) {
-                      String data =  "Process ID: " + pcb.getProcessID() +
-                             ", Process Priority: " + pcb.getProcessPriority() +
-                             ", Arrival Time: " + pcb.getArrivalTime()+"ms" +
-                             ", CPU Burst Time: " + pcb.getCpuBurstTime() +"ms" +
-                             ", Starting Time: " + pcb.getStartingTime()+"ms" +
-                             ", Termination Time: " + pcb.getTerminationTime()+"ms" +
-                             ", Turnaround Time: " + pcb.getTurnAroundTime()+"ms" +
-                             ", Waiting Time: " + pcb.getWaitingTime()+"ms" +
-                             ", Response Time: " + pcb.getPerformanceTime() +"ms";
-                      bufferedWriter.write(data);
-                      bufferedWriter.newLine();
+                for (PCB pcb : queue2) {
+                    String data =  "Process ID: " + pcb.getProcessID() +
+                            ", Process Priority: " + pcb.getProcessPriority() +
+                            ", Arrival Time: " + pcb.getArrivalTime()+"ms" +
+                            ", CPU Burst Time: " + pcb.getCpuBurstTime() +"ms" +
+                            ", Starting Time: " + pcb.getStartingTime()+"ms" +
+                            ", Termination Time: " + pcb.getTerminationTime()+"ms" +
+                            ", Turnaround Time: " + pcb.getTurnAroundTime()+"ms" +
+                            ", Waiting Time: " + pcb.getWaitingTime()+"ms" +
+                            ", Response Time: " + pcb.getPerformanceTime() +"ms";
+                    bufferedWriter.write(data);
+                    bufferedWriter.newLine();
                       System.out.println(data); // Print to console
-                      totalTurnaround += pcb.getTurnAroundTime();
-                      totalWaitingTime += pcb.getWaitingTime();
-                      totalResponseTime += pcb.getPerformanceTime();
-                  }//end for
-                  
+                    totalTurnaround += pcb.getTurnAroundTime();
+                    totalWaitingTime += pcb.getWaitingTime();
+                    totalResponseTime += pcb.getPerformanceTime();
+                }//end for
+                
                   //Processes average calculations
-                  double averTurnaround = totalTurnaround/numberOfProcesses;
-                  double averWaitingTime = totalWaitingTime/numberOfProcesses;
-                  double averResponseTime = totalResponseTime/numberOfProcesses;
-                  String calculation = "Average turnaround time for all processes in the system is : "+ averTurnaround +"ms\n" +
+                double averTurnaround = totalTurnaround/numberOfProcesses;
+                double averWaitingTime = totalWaitingTime/numberOfProcesses;
+                double averResponseTime = totalResponseTime/numberOfProcesses;
+                String calculation = "Average turnaround time for all processes in the system is : "+ averTurnaround +"ms\n" +
                             "Average waiting time for all processes in the system is : "+averWaitingTime +"ms\n"
                             +"Average response time for all processes in the system is : "+averResponseTime +"ms\n";
-                  bufferedWriter.write(calculation);
-                 bufferedWriter.newLine();
-                 System.out.println(calculation);
-                  
+                bufferedWriter.write(calculation);
+                bufferedWriter.newLine();
+                System.out.println(calculation);
+                
                   // Close the Report file
-                  bufferedWriter.close();
-              
-              } catch (IOException e) {
-                  System.err.println("Error writing to file: " + e.getMessage());
-              }//end catch
-          
-          
-     }//end report method
+                bufferedWriter.close();
+            
+            } catch (IOException e) {
+                System.err.println("Error writing to file: " + e.getMessage());
+            }//end catch
+        
+        
+    }//end report method
 
 }//end driver class
