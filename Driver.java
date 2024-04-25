447226319e502d967246eceaac026b24f34ad88c
DanaHafez <156857033+2DH0@users.noreply.github.com>
2024-04-26 00:23:58 +0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -277,3 +277,57 @@ for (PCB process : readyQueue) {
 
 }//end driver class
 
+//dana 
+public static void processesReport() {
+    double totalTurnaround = 0;
+    double totalWaitingTime = 0;
+    double totalResponseTime = 0;
+    double numberOfProcesses = cpuExecutionQueue.size();
+
+    try {
+        // Delete the existing file if it exists
+        File file = new File("Report.txt");
+        if (file.exists()) {
+            file.delete();
+        }
+        // Open the file for writing
+        FileWriter fileWriter = new FileWriter("Report.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        // Write and print data from cpuExecutionQueue
+        for (PCB pcb : cpuExecutionQueue) {
+            String data = "Process ID: " + pcb.getProcessID() +
+                    ", Process Priority: " + pcb.getProcessPriority() +
+                    ", Arrival Time: " + pcb.getArrivalTime() + "ms" +
+                    ", CPU Burst Time: " + pcb.getCpuBurstTime() + "ms" +
+                    ", Starting Time: " + pcb.getStartingTime() + "ms" +
+                    ", Termination Time: " + pcb.getTerminationTime() + "ms" +
+                    ", Turnaround Time: " + pcb.getTurnAroundTime() + "ms" +
+                    ", Waiting Time: " + pcb.getWaitingTime() + "ms" +
+                    ", Response Time: " + pcb.getPerformanceTime() + "ms";
+            bufferedWriter.write(data);
+            bufferedWriter.newLine();
+            System.out.println(data); // Print to console
+            totalTurnaround += pcb.getTurnAroundTime();
+            totalWaitingTime += pcb.getWaitingTime();
+            totalResponseTime += pcb.getPerformanceTime();
+        }
+
+        // Processes average calculations
+        double averTurnaround = totalTurnaround / numberOfProcesses;
+        double averWaitingTime = totalWaitingTime / numberOfProcesses;
+        double averResponseTime = totalResponseTime / numberOfProcesses;
+        String calculation = "Average turnaround time for all processes in the system is : " + averTurnaround + "ms\n" +
+                "Average waiting time for all processes in the system is : " + averWaitingTime + "ms\n"
+                + "Average response time for all processes in the system is : " + averResponseTime + "ms\n";
+        bufferedWriter.write(calculation);
+        bufferedWriter.newLine();
+        System.out.println(calculation);
+
+        // Close the Report file
+        bufferedWriter.close();
+
+    } catch (IOException e) {
+        System.err.println("Error writing to file: " + e.getMessage());
+    }//end catch
+}
